d1f2278fe65afea3e32a6f92c16877a05df2276b
Alex <alexamuek@gmail.com>
2025-08-12 17:51:34 +0800

    increase timeout

--- tests/tasksFilters.spec.js
+++ tests/tasksFilters.spec.js
@@ -29,7 +29,7 @@ test.afterEach(async ({ page }) => {
 
 test.describe('Positive Tests', () => {
   test('filter by assignee', async () => {
-    test.setTimeout(10000);
+    test.setTimeout(15000);
     await tasksPage.filterByAssignee()
     await tasksPage.checkFilterResult(taskId, taskCount)
     await tasksPage.removeFilterByAssignee()
@@ -37,7 +37,7 @@ test.describe('Positive Tests', () => {
   })
 
   test('filter by status', async () => {
-    test.setTimeout(10000);
+    test.setTimeout(15000);
     await tasksPage.filterByStatus()
     await tasksPage.checkFilterResult(taskId, taskCount)
     await tasksPage.removeFilterByStatus()
@@ -45,7 +45,7 @@ test.describe('Positive Tests', () => {
   })
 
   test('filter by label', async () => {
-    test.setTimeout(10000);
+    test.setTimeout(15000);
     await tasksPage.filterByLabel()
     await tasksPage.checkFilterResult(taskId, taskCount)
     await tasksPage.removeFilterByLabel()
